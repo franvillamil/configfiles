f957ac4a0088b3c5089ab59be29143034a3f4be6
Fran Villamil <franvillamil@gmail.com>
2021-11-03 09:46:16 +0100

    update

--- sym.sh
+++ sym.sh
@@ -1,2 +1,4 @@
+#!/usr/bin/env bash
 rm ~/.zshrc && ln -s ~/configfiles/.zshrc ~/.zshrc
 rm ~/.p10k.zsh && ln -s ~/configfiles/.p10k.zsh ~/.p10k.zsh
+rm ~/.gitconfig && ln -s configfiles/.gitconfig  ~/.gitconfig
