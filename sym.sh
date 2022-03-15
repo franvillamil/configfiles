425fc0d42d8b871bc019b24a135427a92c4b84e9
Fran Villamil <franvillamil@gmail.com>
2022-03-15 10:56:39 +0100

    commenting out atom - using syncsettings instead

--- sym.sh
+++ sym.sh
@@ -12,8 +12,8 @@ rm ~/.p10k.zsh && ln -s ~/configfiles/.p10k.zsh ~/.p10k.zsh
 rm ~/.gitconfig && ln -s configfiles/.gitconfig  ~/.gitconfig
 
 # Atom
-rm ~/.atom/config.cson && ln -s ~/configfiles/atom/init.coffee ~/.atom/config.cson
-rm ~/.atom/init.coffee && ln -s ~/configfiles/atom/init.coffee ~/.atom/init.coffee
-rm ~/.atom/keymap.cson && ln -s ~/configfiles/atom/keymap.cson ~/.atom/keymap.cson
-rm ~/.atom/snippets.cson && ln -s ~/configfiles/atom/snippets.cson ~/.atom/snippets.cson
-rm ~/.atom/styles.less && ln -s ~/configfiles/atom/styles.less ~/.atom/styles.less
+#rm ~/.atom/config.cson && ln -s ~/configfiles/atom/init.coffee ~/.atom/config.cson
+#rm ~/.atom/init.coffee && ln -s ~/configfiles/atom/init.coffee ~/.atom/init.coffee
+#rm ~/.atom/keymap.cson && ln -s ~/configfiles/atom/keymap.cson ~/.atom/keymap.cson
+#rm ~/.atom/snippets.cson && ln -s ~/configfiles/atom/snippets.cson ~/.atom/snippets.cson
+#rm ~/.atom/styles.less && ln -s ~/configfiles/atom/styles.less ~/.atom/styles.less
