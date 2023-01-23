89a669f618294f5d4787c9ef55051029513d6024
Fran Villamil <franvillamil@gmail.com>
2023-01-23 15:03:18 +0100

    modified R profile and changed pandoc function

--- .Rprofile
+++ .Rprofile
@@ -1,5 +1,11 @@
 # Default options
 options(scipen = 999, stringsAsFactors = FALSE)
 
+# Default CRAN mirror
+options(repos=structure(c(CRAN="https://cloud.r-project.org/")))
+
+# Default library path
+# .libPaths(c("/Library/Frameworks/R.framework/Versions/4.2/Resources/library"))
+
 # Shortcuts for functions
 adf = function(x){as.data.frame(x)}
--- func/pandoc_docx_to_pdf.sh
+++ func/pandoc_docx_to_pdf.sh
@@ -1,1 +1,1 @@
-find ./ -iname '*.docx' -type f -exec sh -c 'pandoc "${0}" -o "${0%.docx}.pdf" -V geometry:margin=0.3in --pdf-engine=xelatex -V fontsize=11pt;rm "${0}"' {} \;
+find ./ -iname '*.docx' -type f -exec sh -c 'pandoc "${0}" -o "${0%.docx}.pdf" -V geometry:margin=0.75in --pdf-engine=xelatex -V fontsize=12pt -V linestretch=1.2;rm "${0}"' {} \;
