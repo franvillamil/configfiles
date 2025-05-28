c20f416948f2331c798f6a799fea45044a55d5aa
Fran Villamil <franvillamil@gmail.com>
2025-05-28 15:26:07 +0200

    autoupdate

--- func/gitoverleaf.sh
+++ func/gitoverleaf.sh
@@ -1,6 +1,6 @@
-cleantex
 rm -vf main.pdf appendix.pdf response_memo.pdf
-rm -vf main.aux appendix.aux response_memo.aux
+rm -vf main.aux response_memo.aux
+rm -rvf *.toc *.log *.out *.bbl *.blg *.fdb_latexmk *.thm *.fls *.synctex.gz *.lof *.lot *.bcf *.nav *.run.xml *.snm
 git add .
 git commit -m "update"
 git push
