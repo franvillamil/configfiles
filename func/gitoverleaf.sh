ed0130c0e7c93acee06d7668f1a18e5600b6b179
Francisco Villamil <franvillamil@gmail.com>
2025-06-15 19:38:56 +0200

    autoupdate

--- func/gitoverleaf.sh
+++ func/gitoverleaf.sh
@@ -1,4 +1,4 @@
-rm -vf main.pdf appendix.pdf response_memo.pdf
+rm -vf main.pdf response_memo.pdf
 rm -vf main.aux response_memo.aux
 rm -rvf *.toc *.log *.out *.bbl *.blg *.fdb_latexmk *.thm *.fls *.synctex.gz *.lof *.lot *.bcf *.nav *.run.xml *.snm
 git add .
