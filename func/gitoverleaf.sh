753f5efe98783e60e2ae6c6bcc6c027655857254
Francisco Villamil <franvillamil@gmail.com>
2026-04-20 13:34:27 +0200

    autoupdate

--- func/gitoverleaf.sh
+++ func/gitoverleaf.sh
@@ -1,6 +1,7 @@
 rm -vf main.pdf response_memo.pdf
 rm -vf main.aux response_memo.aux
-rm -rvf *.toc *.log *.out *.bbl *.blg *.fdb_latexmk *.thm *.fls *.synctex.gz *.lof *.lot *.bcf *.nav *.run.xml *.snm
+rm -rvf *.toc *.log *.out *.bbl *.blg *.fdb_latexmk *.thm *.fls *.synctex.gz *.lof *.lot *.bcf *.nav *.run.xml *.snm *.pgf
+rm -f .DS_Store
 git add .
 git commit -m "update"
 git push
